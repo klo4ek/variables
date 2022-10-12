eff0a3928bb2c0e939116210cf2b403c4513ae75
KontiVL <kokajama10@gmail.com>
2022-10-12 22:19:35 +0700

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,26 @@
+package bubles.date.month
+
+fun main()  {
+//    val c: Char = 0.toChar()// + 2
+
+//    println('a')//.toInt())
+//    println(('a' +1))//.toInt())
+//    println(c)
+//    println(c.toInt())
+
+    val double = 1.0
+    val float = 1.0F
+
+    println(double)
+    println(double::class)
+    println(float)
+    println(float::class)
+
+    val integer = 123
+    val long = 123L
+
+    println(integer)
+    println(integer::class)
+    println(long)
+    println(long::class)
+}
